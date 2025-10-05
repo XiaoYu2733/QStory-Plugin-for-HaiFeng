e6b184f075c376b6eb28386b2acde231a5445e1e
海枫 <147979341+XiaoYu2733@users.noreply.github.com>
2025-10-05 16:22:47 +0800

    更新 艾特自动回复以及禁言.java
    - [移除] 部分特性
    - [移除] 更新日志弹窗
    - [更改] 部分名称

--- 艾特自动回复以及禁言.java
+++ 艾特自动回复以及禁言.java
@@ -3,46 +3,12 @@
 
 // 眼泪是人最纯真的东西 流尽了人就变得冷漠了
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.graphics.Color;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.TimePicker;
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.Calendar;
-
 String CONFIG_NAME = "AutoReplyConfig";
 String FORBIDDEN_CONFIG_NAME = "AutoForbiddenConfig";
-String REPLY_CONTENT = "打倒夜七"; // 艾特回复内容
+String REPLY_CONTENT = "艾特我干嘛"; // 艾特回复内容 自己改
 
-addItem("艾特自动回复开关", "toggleAutoReply");
-addItem("艾特自动禁言开关", "toggleAutoForbidden");
-addItem("脚本本次更新日志","showUpdateLog");
-
-public void showUpdateLog(String g, String u, int t) {
-    Activity activity = getActivity();
-    if (activity == null) return;
-    
-    activity.runOnUiThread(new Runnable() {
-        public void run() {
-            AlertDialog.Builder builder = new AlertDialog.Builder(activity, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
-            builder.setTitle("脚本更新日志");
-            builder.setMessage("海枫qwq\n\n" +
-                    "更新日志\n" +
-                    "适配 最新版QStory\n" +
-                    "反馈交流群：https://t.me/XiaoYu_Chat");
-            builder.setPositiveButton("确定", null);
-            builder.show();
-        }
-    });
-}
+addItem("开启/关闭本群艾特自动回复", "toggleAutoReply");
+addItem("开启/关闭本群艾特自动禁言", "toggleAutoForbidden");
 
 public void toggleAutoReply(String groupUin, String userUin, int chatType) {
     if (chatType == 2) {
@@ -52,6 +18,8 @@ public void toggleAutoReply(String groupUin, String userUin, int chatType) {
     }
 }
 
+// 原来你和谁聊天都那么开心 我恨你
+
 public void toggleAutoForbidden(String groupUin, String userUin, int chatType) {
     if (chatType == 2) {
         boolean current = getBoolean(FORBIDDEN_CONFIG_NAME, groupUin, false);
@@ -59,6 +27,8 @@ public void toggleAutoForbidden(String groupUin, String userUin, int chatType) {
         toast(groupUin + (!current ? " 已开启艾特自动禁言" : " 已关闭艾特自动禁言"));
     }
 }
+
+// 直到后来我看见你爱别人的样子 才发现我从未真正走进你心里
 
 void onMsg(Object msg) {
     if (shouldForbid(msg)) {
@@ -69,6 +39,8 @@ void onMsg(Object msg) {
         sendMsg(msg.GroupUin, "", REPLY_CONTENT);
     }
 }
+
+// 是你先接近我的 也是你先不喜欢我的
 
 boolean shouldReply(Object msg) {
     return msg.IsGroup &&
