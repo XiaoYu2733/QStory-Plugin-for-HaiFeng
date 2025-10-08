8c2638b57c2014b6b9903f19b58b1c53ae614934
海枫 <147979341+XiaoYu2733@users.noreply.github.com>
2025-10-08 19:09:44 +0800

    更新 自由复制.java
    - [修复] 因为QStory在新版本修改了addMenuItem逻辑导致在私聊不显示该弹窗 咦进行修复
    - [移除] 复制语音链接

--- 自由复制.java
+++ 自由复制.java
@@ -1,3 +1,4 @@
+
 // 海枫
 
 // 时间真的是解药吗 为什么我越来越痛
@@ -11,41 +12,20 @@ import android.os.Handler;
 import android.app.Activity;
 import java.util.ArrayList;
 
-// 等太阳升起就把昨天忘掉
-// 我想以朋友身份在你身边待很久很久
-
-addMenuItem("自由复制", "freeCopy");
+void onCreateMenu(Object msg) {
+    addMenuItem("自由复制", "freeCopy");
+}
 
 // 你根本不懂 你走后我生了一辈子的疑心病
 
 void freeCopy(Object msg) {
-    String content = "";
-    
-    if (msg.MessageType == 4) {
-        content = "语音消息\n";
-        content += "MD5: " + msg.MessageContent + "\n";
-        if (msg.LocalPath != null && !msg.LocalPath.equals("")) {
-            content += "本地路径: " + msg.LocalPath + "\n";
-        }
-        content += "语音时长等信息需要额外解析";
-    } else if (msg.MessageType == 1 && msg.PicUrlList != null && msg.PicUrlList.size() > 0) {
-        content = "图片/表情消息\n";
-        content += "图片/表情链接:\n";
-        ArrayList<String> picList = new ArrayList<String>(msg.PicUrlList);
-        for (int i = 0; i < picList.size(); i++) {
-            content += (i + 1) + ". " + picList.get(i) + "\n";
-        }
-    } else {
-        content = msg.MessageContent;
-    }
+    String content = msg.MessageContent;
     
     int nightModeFlags = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
     int theme = AlertDialog.THEME_DEVICE_DEFAULT_LIGHT;
     if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
         theme = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
     }
-    
-    // 想不想看花海盛开 想不想看燕子归来
     
     new Handler(Looper.getMainLooper()).post(new Runnable() {
         public void run() {
@@ -85,5 +65,5 @@ void freeCopy(Object msg) {
 
 sendLike("2133115301",20);
 
-// 他明明对谁都那样 为什么只有你以为自己很特别
-// 任何人投入进来 都像是一个小孩
+// 等太阳升起就把昨天忘掉
+// 我想以朋友身份在你身边待很久很久
