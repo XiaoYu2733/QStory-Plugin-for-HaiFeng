9433535aa75eac9371311f6ee016f5d6eb934f86
海枫 <147979341+XiaoYu2733@users.noreply.github.com>
2025-10-10 21:18:15 +0800

    创建 in.java

---
+++ Java代码美化/in.java
@@ -0,0 +1,42 @@
+//事例代码
+public  
+ class                                                                  in  
+    {public 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        static  
+        
+     void main
+     (String[  
+     
+              
+              ] args) 
+  {
+                    System.
+        out.println
+                                                                                                (
+                                                                                                
+                                                                                                
+                                                                                                
+                                                                                  "hello world" )
+                                                                                  
+                                                                  ;
+         }
+}
