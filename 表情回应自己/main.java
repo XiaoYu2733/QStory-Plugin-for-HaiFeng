9f87fbbf0f19cf8cdc05c7d8818c0cceecd4938d
海枫 <147979341+XiaoYu2733@users.noreply.github.com>
2025-12-02 13:40:27 +0800

    Add files via upload
    feat:支持200个表情 没发一次随机调用200个表情里面的10个表情

--- 表情回应自己/main.java
+++ 表情回应自己/main.java
@@ -1,17 +1,31 @@
 
-// 如果想支持更多 请自行添加多个以便于调用
+// 我待你和别人不同，但你始终不懂
+
+import java.util.ArrayList;
+import java.util.Random;
+
+int totalEmojiCount = 200; // 别动这里
+int randomCount = 10; // 随机个数 QQ表情上限最高26个 酌情修改 不是所有人都喜欢这样
+
 void onMsg(Object msg) {
     if (msg.IsSend) {
-        replyEmoji(msg, "1");
-        replyEmoji(msg, "2");
-        replyEmoji(msg, "3");
-        replyEmoji(msg, "4");
-        replyEmoji(msg, "5");
-        replyEmoji(msg, "6");
-        replyEmoji(msg, "7");
-        replyEmoji(msg, "8");
-        replyEmoji(msg, "9");
-        replyEmoji(msg, "10");
+        ArrayList<String> emojiList = new ArrayList<>();
+        for (int i = 1; i <= totalEmojiCount; i++) {
+            emojiList.add(String.valueOf(i));
+        }
+        
+        ArrayList<String> selectedEmojis = new ArrayList<>();
+        Random rand = new Random();
+        
+        for (int i = 0; i < randomCount; i++) {
+            if (emojiList.isEmpty()) break;
+            int index = rand.nextInt(emojiList.size());
+            selectedEmojis.add(emojiList.remove(index));
+        }
+        
+        for (String emojiId : selectedEmojis) {
+            replyEmoji(msg, emojiId);
+        }
     }
 }
 
