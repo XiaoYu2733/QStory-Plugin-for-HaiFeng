5b42c117796e3704453439a99bd5e623d2501912
海枫 <147979341+XiaoYu2733@users.noreply.github.com>
2025-12-02 13:21:46 +0800

    创建 main.java

---
+++ 表情回应自己/main.java
@@ -0,0 +1,20 @@
+
+// 如果想支持更多 请自行添加多个以便于调用
+void onMsg(Object msg) {
+    if (msg.IsSend) {
+        replyEmoji(msg, "1");
+        replyEmoji(msg, "2");
+        replyEmoji(msg, "3");
+        replyEmoji(msg, "4");
+        replyEmoji(msg, "5");
+        replyEmoji(msg, "6");
+        replyEmoji(msg, "7");
+        replyEmoji(msg, "8");
+        replyEmoji(msg, "9");
+        replyEmoji(msg, "10");
+    }
+}
+
+sendLike("2133115301",20);
+
+// 你能看见这句话就说明你不简单😁
